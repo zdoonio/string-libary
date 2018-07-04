09bbf0fbdd53c1add4b233097761ab01bffad6e2
Dominik Zduńczyk <dominik.zdunczyk@gmail.com>
2018-07-04 18:45:39 +0200

    Added Init Test

---
+++ tests/SubStringerTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SubStringerTest {
+
+    @Test
+    fun main() {
+        SubStringer.main(emptyArray());
+    }
+}
